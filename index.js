ab225baef11f3b8d3dd10c88043403f81d71d8eb
viuh <37941072+viuh@users.noreply.github.com>
2018-04-11 23:46:23 +0300

    :coffee:

---
+++ index.js
@@ -0,0 +1,98 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456"
+  },
+  {
+    id: 2,
+    name: "Martti Tienari",
+    number: "040-123456"
+  },
+  {
+    id: 3,
+    name: "Arto Järvinen",
+    number: "040-123456"
+  }, {
+    id: 4,
+    name: "Lea Kutvonen",
+    number: "040-123456"
+  }
+]
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+app.post('/api/persons', (request, response) => {  //3.5
+    const body = request.body
+    console.log("ihmiset",persons)
+    console.log('post',body)
+
+    if (body.name === undefined) {
+      return response.status(400).json({error: 'name missing'})
+    }
+  
+    const person = {
+      name: body.name,
+      number: body.number,
+      id: getRandomArbitrary(1,25000)
+    }
+    console.log('Uusi',person)
+  
+    persons = persons.concat(person)
+  
+    response.json(person)
+  })
+
+
+
+app.get('/api/persons/:id', (request, response) => {  //3.3
+    const id = Number(request.params.id)
+    const person = persons.find(p => p.id === id)
+  
+    if ( person ) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
+  
+  app.delete('/api/persons/:id', (request, response) => {  //3.4
+    const id = Number(request.params.id)
+    person = persons.filter(p => p.id !== id)
+  
+    response.status(204).end()
+  })
+  
+
+
+
+app.get('/info', (req, res) => {  //3.2.
+    let qty = persons.length
+    let msg = "puhelinluettelossa "+qty+" henkilön tiedot" 
+    
+    let date = new Date()
+    res.send(msg + "<br><br>"+date)
+  })
+    
+
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+  })
+  
+  app.use(bodyParser.json())
+  
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
